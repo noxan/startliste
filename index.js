799fbf0ca34ae6c1ff8bcacc69e306ade908777c
Richard <hello@noxan.net>
2017-09-15 17:06:59 +0200

    read file from argv params

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const { argv } = process;
+
+if (argv.length < 3) {
+    console.log('Please provice a file as argument.');
+    process.exit(1);
+}
+
+const filename = argv[2];
+
+console.log(filename);
+
+const content = fs.readFileSync(filename, { encoding: 'utf8' });
+
+const lines = content.split('\n');
+
+console.log(lines);
